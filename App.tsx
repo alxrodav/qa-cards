eb530847f7ce69fa0881644483a1e0bc9df93ad4
Alexey Rodriguez Avila <alxrodav@gmail.com>
2021-07-23 17:44:14 +0200

    Setup: ESLint & Prettier

--- App.tsx
+++ App.tsx
@@ -2,20 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { colors } from './src/theme';
+
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		alignItems: 'center',
+		backgroundColor: colors.white,
+		flex: 1,
+		justifyContent: 'center',
+	},
 });
+
+export default App;
